1e281092e0ca7bae97d4604f3c496b8dacdad78f
maksonday <maksim.agafonofff@gmail.com>
2022-03-27 21:29:18 +0300

    added mockking and call count check

--- src/test/kotlin/ru/tinkoff/fintech/homework/lesson1/ArmyTest.kt
+++ src/test/kotlin/ru/tinkoff/fintech/homework/lesson1/ArmyTest.kt
@@ -1,8 +1,9 @@
 package ru.tinkoff.fintech.homework.lesson1
 
+import io.mockk.every
 import io.mockk.junit5.MockKExtension
+import io.mockk.mockk
 import io.mockk.verify
-import io.mockk.spyk
 import org.junit.jupiter.api.Assertions.assertAll
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.extension.ExtendWith
@@ -13,76 +14,77 @@ import org.junit.jupiter.params.provider.CsvSource
 internal class ArmyTest {
     @ParameterizedTest
     @CsvSource("1, 1.0, 3.0, 14.0", "2, 2.0, 6.0, 23.0")
-    fun `тестируем army getBuff с count с помощью spy`(count : Int, initCharacteristic : Double, mainCharacteristic : Double, attackDmg : Double) {
-        val warrior = WarriorUnit("human")
-        warrior.strength = initCharacteristic
-        val archer = ArcherUnit("elf")
-        archer.agility = initCharacteristic
-        val mage = MageUnit("gnome")
-        mage.intelligence = initCharacteristic
-        val army = Army("OurArmy", listOf(warrior, archer, mage))
-        val spyArmy = spyk(army)
+    fun `тестируем army getBuff с count с помощью mockk`(
+        count: Int,
+        initCharacteristic: Double,
+        mainCharacteristic: Double,
+        attackDmg: Double
+    ) {
+        val warrior = mockk<WarriorUnit>(relaxed = true)
+        every {
+            warrior.strength
+        } answers { fieldValue }
+        every {
+            warrior.strength = any()
+        } propertyType Double::class answers { fieldValue += value + initCharacteristic }
+        every {
+            warrior.agility
+        } answers { fieldValue + 1.0 }
+        every {
+            warrior.intelligence
+        } answers { fieldValue + 1.0 }
+        every {
+            warrior.attackDamage
+        } answers { attackDmg }
+        val army = Army("myArmy", listOf(warrior))
+
+        army.getBuff(count)
 
         assertAll(
-            { assertEquals(Unit, spyArmy.getBuff(count)) },
-            { verify(exactly = 1) { spyArmy.getBuff(count) } },
+            { verify(exactly = 1) { army.getBuff(count) } },
             {
                 assertEquals(mainCharacteristic, warrior.strength)
-                assertEquals(1.0,  warrior.agility)
+                assertEquals(1.0, warrior.agility)
                 assertEquals(1.0, warrior.intelligence)
-            },
-            {
-                assertEquals(1.0, archer.strength)
-                assertEquals(mainCharacteristic, archer.agility)
-                assertEquals(1.0, archer.intelligence)
-            },
-            {
-                assertEquals(1.0, mage.strength)
-                assertEquals(1.0, mage.agility)
-                assertEquals(mainCharacteristic, mage.intelligence)
-            },
-            {
                 assertEquals(attackDmg, warrior.attackDamage)
-                assertEquals(attackDmg, archer.attackDamage)
-                assertEquals(attackDmg, mage.attackDamage)
             }
         )
     }
 
     @ParameterizedTest
     @CsvSource("1.0, 3.0, 14.0", "2.0, 4.0, 15.0")
-    fun `тестируем army getBuff с помощью spy`(initCharacteristic : Double, mainCharacteristic : Double, attackDmg : Double) {
-        val warrior = WarriorUnit("human")
-        warrior.strength = initCharacteristic
-        val archer = ArcherUnit("elf")
-        archer.agility = initCharacteristic
-        val mage = MageUnit("gnome")
-        mage.intelligence = initCharacteristic
-        val army = Army("OurArmy", listOf(warrior, archer, mage))
-        val spyArmy = spyk(army)
+    fun `тестируем army getBuff с помощью mockk`(
+        initCharacteristic: Double,
+        mainCharacteristic: Double,
+        attackDmg: Double
+    ) {
+        val warrior = mockk<WarriorUnit>(relaxed = true)
+        every {
+            warrior.strength
+        } answers { fieldValue }
+        every {
+            warrior.strength = any()
+        } propertyType Double::class answers { fieldValue += value + initCharacteristic }
+        every {
+            warrior.agility
+        } answers { fieldValue + 1.0 }
+        every {
+            warrior.intelligence
+        } answers { fieldValue + 1.0 }
+        every {
+            warrior.attackDamage
+        } answers { attackDmg }
+        val army = Army("myArmy", listOf(warrior))
+
+        army.getBuff()
 
         assertAll(
-            { assertEquals(Unit, spyArmy.getBuff()) },
-            { verify(exactly = 1) { spyArmy.getBuff() } },
+            { verify(exactly = 1) { army.getBuff() } },
             {
                 assertEquals(mainCharacteristic, warrior.strength)
-                assertEquals(1.0,  warrior.agility)
+                assertEquals(1.0, warrior.agility)
                 assertEquals(1.0, warrior.intelligence)
-            },
-            {
-                assertEquals(1.0, archer.strength)
-                assertEquals(mainCharacteristic, archer.agility)
-                assertEquals(1.0, archer.intelligence)
-            },
-            {
-                assertEquals(1.0, mage.strength)
-                assertEquals(1.0, mage.agility)
-                assertEquals(mainCharacteristic, mage.intelligence)
-            },
-            {
                 assertEquals(attackDmg, warrior.attackDamage)
-                assertEquals(attackDmg, archer.attackDamage)
-                assertEquals(attackDmg, mage.attackDamage)
             }
         )
     }
@@ -90,22 +92,17 @@ internal class ArmyTest {
 
     @ParameterizedTest
     @CsvSource("1, 12.0", "2, 13.0")
-    fun `тестируем army incAttackDamage с помощью spy`(count : Int, attackDmg : Double){
-        val warrior = WarriorUnit("human")
-        val archer = ArcherUnit("elf")
-        val mage = MageUnit("gnome")
-        val army = Army("OurArmy", listOf(warrior, archer, mage))
-        val spyArmy = spyk(army)
+    fun `тестируем army incAttackDamage с помощью mockk`(count: Int, attackDmg: Double) {
+        val warrior = mockk<WarriorUnit>(relaxed = true)
+        every {
+            warrior.attackDamage
+        } answers { attackDmg }
+        val army = Army("myArmy", listOf(warrior))
 
-        assertAll(
-            { assertEquals(Unit, spyArmy.incAttackDamage(count)) },
-            { verify(exactly = 1) { spyArmy.incAttackDamage(count) } },
-            {
-                assertEquals(attackDmg, warrior.attackDamage)
-                assertEquals(attackDmg, archer.attackDamage)
-                assertEquals(attackDmg, mage.attackDamage)
-            }
-        )
+        army.incAttackDamage(count)
+
+        verify(exactly = 1) { army.incAttackDamage(count) }
+        assertEquals(attackDmg, warrior.attackDamage)
     }
 
 }
--- src/test/kotlin/ru/tinkoff/fintech/homework/lesson1/WarriorTest.kt
+++ src/test/kotlin/ru/tinkoff/fintech/homework/lesson1/WarriorTest.kt
@@ -1,13 +1,18 @@
 package ru.tinkoff.fintech.homework.lesson1
 
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Assertions.assertAll
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
 
-internal class WarriorTest{
+internal class WarriorTest {
     @ParameterizedTest
     @CsvSource("5, 11.0, 66.0", "10, 21.0, 221.0")
-    fun `когда воин получает бафф, должны увеличиться его сила и урон от атаки`(count : Int, strength : Double, attackDmg : Double){
+    fun `когда воин получает бафф, должны увеличиться его сила и урон от атаки`(
+        count: Int,
+        strength: Double,
+        attackDmg: Double
+    ) {
         val warrior = WarriorUnit("human")
 
         warrior.getBuff(count)
